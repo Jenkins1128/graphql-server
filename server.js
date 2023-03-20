4482be1cc96a8707440308ad5cd26721da21d82c
Isaiah Jenkins <ij27900@gmail.com>
2023-03-20 16:24:36 -0700

    Adding express graphql server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const {loadFilesSync} = require('@graphql-tools/load-files')
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+
+const typesArray = loadFilesSync('**/*', {
+    extensions: ['graphql']
+});
+
+const resolversArray = loadFilesSync('**/*', {
+    extensions: ['resolvers.js']
+});
+
+const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray
+})
+
+const root = {
+    products: require('./products/products.model'),
+    orders: require('./orders/orders.model')
+}
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(3000, () => {
+    console.log('Running GraphQL server...', );
+})
